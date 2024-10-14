c05fb55ead4dc0f066ee5d62d2159972f4556406
Siddhant <siddhantgodwani8697@gmail.com>
2024-10-14 23:25:20 +0530

    Inputs and taking inputs added

--- KK/Inputs.java
+++ KK/Inputs.java
@@ -1,14 +1,27 @@
 // DSA 1 KK
 
+package KK;
+
 import java.util.Scanner;
 
-public class Taking_inputs{
+public class Inputs{
 public static void main(String args[]){
     // Taking integer input from user using Scanner class in Java
     Scanner input = new Scanner(System.in);
+   
     System.out.println("Enter a int input:");
     int number = input.nextInt();
     System.out.println("You enterred :"+ number);
+
+    // System.out.println("Enter a Line");
+    // String Line = input.nextLine();
+    // System.out.println(Line);
+
+    // System.out.print("Enter a word");
+    // String Word = input.next();
+    // System.out.println(Word);
+
+
 
     }
 }
